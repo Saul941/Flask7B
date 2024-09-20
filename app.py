d8dc86885e268f5ce72672c262c42da2a9eebc5a
Saul <147740984+Saul941@users.noreply.github.com>
2024-09-20 17:55:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,64 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, request, jsonify
 import pusher
-
 import mysql.connector
 import datetime
 import pytz
 
-con = mysql.connector.connect(
-  host="185.232.14.52",
-  database="u760464709_tst_sep",
-  user="u760464709_tst_sep_usr",
-  password="dJ0CIAFF="
-)
-
 app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="185.232.14.52",
+        database="u760464709_tst_sep",
+        user="u760464709_tst_sep_usr",
+        password="dJ0CIAFF="
+    )
 
 @app.route("/")
 def index():
-    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
-    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    con.close()
-    matricula      = request.form["txtMatriculaFA"]
+    matricula = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
 @app.route("/buscar")
 def buscar():
-    if not con.is_connected():
-        con.reconnect()
+    con = get_db_connection()
     cursor = con.cursor()
     cursor.execute("SELECT * FROM tst0_cursos ORDER BY Id_Curso DESC")
-
     registros = cursor.fetchall()
     con.close()
-
-    return registros
+    return jsonify(registros)
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
     args = request.args
-
-    if not con.is_connected():
-        con.reconnect()
+    con = get_db_connection()
     cursor = con.cursor()
-
-    sql = "INSERT INTO tst0_cursos ( Nombre_Curso, Telefono, Fecha_Hora) VALUES (%s, %s, %s)"
+    
+    sql = "INSERT INTO tst0_cursos (Nombre_Curso, Telefono, Fecha_Hora) VALUES (%s, %s, %s)"
     val = (args["Nombre_Curso"], args["Telefono"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
     cursor.execute(sql, val)
-    
     con.commit()
     con.close()
 
     pusher_client = pusher.Pusher(
-        app_id = "1868456"
-        key = "2a2b4a22ea18bf23113a"
-        secret = "62943ef022cd594bdde4"
-        cluster = "mt1"
+        app_id="1868456",
+        key="2a2b4a22ea18bf23113a",
+        secret="62943ef022cd594bdde4",
+        cluster="mt1",
         ssl=True
     )
 
     pusher_client.trigger("canalRegistroscursos", "registrocursos", args)
     return args
+
+if __name__ == "__main__":
+    app.run(debug=True)
