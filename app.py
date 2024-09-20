65fbc8535836a2d6e13d113cc74d1addf3ad00ba
Saul <147740984+Saul941@users.noreply.github.com>
2024-09-20 17:47:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,72 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hola, Mundo!</p>"
+    con.close()
+    return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
-def alumnosGuardar(): 
-    matricula = request.form["txtMatriculaFA"]
+def alumnosGuardar():
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM tst0_cursos ORDER BY Id_Curso DESC")
+
+    registros = cursor.fetchall()
+    con.close()
+
+    return registros
+
+@app.route("/registrar", methods=["GET"])
+def registrar():
+    args = request.args
+
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+
+    sql = "INSERT INTO tst0_cursos ( Nombre_Curso, Telefono, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["Nombre_Curso"], args["Telefono"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    cursor.execute(sql, val)
+    
+    con.commit()
+    con.close()
+
+    pusher_client = pusher.Pusher(
+        app_id = "1868456"
+        key = "2a2b4a22ea18bf23113a"
+        secret = "62943ef022cd594bdde4"
+        cluster = "mt1"
+        ssl=True
+    )
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
+    return args
