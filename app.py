e235bb7817023a1c4bf9d0b9d516b633023aeed3
Saul <147740984+Saul941@users.noreply.github.com>
2024-09-23 20:05:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,83 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+
+from flask import render_template
+from flask import request
+
 import pusher
+
 import mysql.connector
 import datetime
 import pytz
 
-app = Flask(__name__)
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
+)
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host="185.232.14.52",
-        database="u760464709_tst_sep",
-        user="u760464709_tst_sep_usr",
-        password="dJ0CIAFF="
-    )
+app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
+
     return render_template("app.html")
 
+# Ejemplo de ruta GET usando templates para mostrar una vista
 @app.route("/alumnos")
 def alumnos():
+    con.close()
+
     return render_template("alumnos.html")
 
+# Ejemplo de ruta POST para ver cómo se envia la informacion
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
+
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
+# Código usado en las prácticas
 @app.route("/buscar")
 def buscar():
-    con = get_db_connection()
+    if not con.is_connected():
+        con.reconnect()
+
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM tst0_cursos ORDER BY Id_Curso DESC")
+    cursor.execute("SELECT * FROM sensor_log ORDER BY Id_Log DESC")
     registros = cursor.fetchall()
+
     con.close()
-    return jsonify(registros)
+
+    return registros
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
     args = request.args
-    con = get_db_connection()
+
+    if not con.is_connected():
+        con.reconnect()
+
     cursor = con.cursor()
-    
-    sql = "INSERT INTO tst0_cursos (Nombre_Curso, Telefono, Fecha_Hora) VALUES (%s, %s, %s)"
-    val = (args["Nombre_Curso"], args["Telefono"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+
+    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
     cursor.execute(sql, val)
+    
     con.commit()
     con.close()
 
     pusher_client = pusher.Pusher(
-        app_id="1868456",
-        key="2a2b4a22ea18bf23113a",
-        secret="62943ef022cd594bdde4",
-        cluster="mt1",
+        app_id="1714541",
+        key="2df86616075904231311",
+        secret="2f91d936fd43d8e85a1a",
+        cluster="us2",
         ssl=True
     )
 
-    pusher_client.trigger("canalRegistroscursos", "registrocursos", args)
-    return args
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return args
