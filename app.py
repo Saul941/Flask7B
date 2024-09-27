bae492d1fa0adecefacfddc3535e3ddc2b91aadd
Saul <147740984+Saul941@users.noreply.github.com>
2024-09-27 18:45:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, request
 import pusher
-
 import mysql.connector
 import datetime
 import pytz
@@ -21,37 +16,30 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     con.close()
-
     return render_template("app.html")
 
-# Ejemplo de ruta GET usando templates para mostrar una vista
 @app.route("/alumnos")
 def alumnos():
     con.close()
-
     return render_template("alumnos.html")
 
-# Ejemplo de ruta POST para ver cómo se envia la informacion
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
     con.close()
-    matricula      = request.form["txtMatriculaFA"]
+    matricula = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
-
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-# Código usado en las prácticas
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
 
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM tst0_cursos ORDER BY Id_Cursos DESC")
+    cursor.execute("SELECT * FROM tst0_cursos ORDER BY ID_curso DESC")
     registros = cursor.fetchall()
 
     con.close()
-
     return registros
 
 @app.route("/registrar", methods=["GET"])
@@ -62,22 +50,21 @@ def registrar():
         con.reconnect()
 
     cursor = con.cursor()
-
-    sql = "INSERT INTO tst0_cursos (Nombre_Curso, Telefono, Fecha_Hora) VALUES (%s, %s, %s)"
-    val = (args["Nombre_Curso"], args["Telefono"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    sql = "INSERT INTO tst0_cursos (ID_curso, Nombre_Curso, Telefono) VALUES (%s, %s, %s)"
+    val = (args["id_curso"], args["nombre_curso"], args["telefono"])
     cursor.execute(sql, val)
-    
+
     con.commit()
     con.close()
 
     pusher_client = pusher.Pusher(
-        app_id='1869686',
-        key='1956602afbf37454f0c8',
-        secret='fb578dc366b244f13147',
-        cluster='mt1',
+        app_id="1714541",
+        key="2df86616075904231311",
+        secret="2f91d936fd43d8e85a1a",
+        cluster="us2",
         ssl=True
     )
 
-    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
+    pusher_client.trigger("canalRegistrosCursos", "registroCurso", args)
 
     return args
